27584023c165e7a7f0b8b8690c84744917ad1cf5
IamGOOD <rukshenas1990@gmail.com>
2023-06-25 22:30:38 -0400

    Refactored home page views for

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,20 +3,25 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from .views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
 
-    def test_root_url_resolves_to_home_page_views(self):
-        '''тест: корневой url преобразуется в представление домашней страницы'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_templates(self):
+        '''тест: использеутся домашний шаблон'''
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
+
 
     def test_home_page_returns_correct_html(self):
         '''тест: домашняя страница возвращает правильный html'''
-        request = HttpRequest()
-        response = home_page(request)
+
+        response = self.client.get('/')
         html = response.content.decode('utf8')
+
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'lists/home.html')
