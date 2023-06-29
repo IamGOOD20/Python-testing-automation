4b57480f2d327f570c0105bf7f9c77be2784ae2b
IamGOOD <rukshenas1990@gmail.com>
2023-06-29 10:21:02 -0400

    The HTML homepage is used from the template now. The functional_tests doen't work now!

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,4 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from .views import home_page
-from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
@@ -19,9 +14,7 @@ class HomePageTest(TestCase):
 
         response = self.client.get('/')
         html = response.content.decode('utf8')
-
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
-
         self.assertTemplateUsed(response, 'lists/home.html')
