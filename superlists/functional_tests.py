53727b5d2922813c3df069ea988c7e6f8f0a8e44
IamGOOD <rukshenas1990@gmail.com>
2023-09-24 00:49:28 -0400

    Refactoring F_T

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -16,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
         '''disconnect Chrome'''
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''Test: you can start the list and continue it later'''
         self.browser.get('http://localhost:8000')
@@ -38,11 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         # the page has "1"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Exchange laziness on brain')
 
-
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Exchange laziness on brain', [row.text for row in rows])
             # f"The new list element did not appear in a table. It consist of:\n{table.text}")
         # The text field is proposing to add another one element
         # She inputs "To make task 2"
@@ -51,13 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Exchange laziness on brain', [row.text for row in rows])
-        self.assertIn(
-            '2: Start working hard',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Exchange laziness on brain')
+        self.check_for_row_in_list_table('2: Start working hard')
 
         self.fail('End test')
         # after page update it views both elements of her list
